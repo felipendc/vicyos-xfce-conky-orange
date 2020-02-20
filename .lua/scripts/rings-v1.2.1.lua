4b5ae18bc39182cfc10152ec6fe8f09e2463a241
felipendc <felipendc10@gmail.com>
2020-02-20 01:34:57 -0300

    Upload Circle overlapping fix for 94mb/s network speed

--- .lua/scripts/rings-v1.2.1.lua
+++ .lua/scripts/rings-v1.2.1.lua
@@ -210,7 +210,7 @@ settings_table = {
     {
         name='upspeedf',
         arg='',
-        max=13700,
+        max=13800,
         bg_colour=0xd9d9d9,
         bg_alpha=0.2,
         fg_colour=0xdd4522,
